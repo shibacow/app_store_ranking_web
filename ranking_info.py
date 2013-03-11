ab92f41ce23df8f204efda305f00d0600984ee43
Ubuntu <ubuntu@ip-10-132-148-24.ap-northeast-1.compute.internal>
2013-03-11 17:48:56 +0000

    ch data

--- ranking_info.py
+++ ranking_info.py
@@ -1,6 +1,6 @@
 import os,sys
 sys.path.append('../app_store_ranking')
-from datetime import datetime
+from datetime import datetime,timedelta
 from mog_op import MongoOp,ObjectId
 import web
 import re
@@ -37,8 +37,9 @@ class RankingInfo(object):
                     img=img['label']
                 self.ranking_data.append(dict(rank=r,title=title,summary=summary,img=img,link=link))
         else:
-            web.debug(type(entries))
-            web.debug(entries)
+            pass
+            #web.debug(type(entries))
+            #web.debug(entries)
     def __init__(self,rr):
         self.country=selector_info.country_list.get(rr['country'],rr['country'])
         feed=rr['feed']
@@ -51,6 +52,8 @@ class Ranking(object):
     def __get_meta_data(self,mp,fd,to,inputs):
         m=inputs['media']
         f=inputs['field']
+        self.media=m
+        self.field=f
         dk={"mediatype":m,"fieldtype":f,\
             "fetch_date":{"$lt":to,"$gte":fd}}
         return mp.find_all(mp.RANKING_META_DATA,dk)
@@ -66,6 +69,7 @@ class Ranking(object):
         to=datetime.strptime(to,'%Y-%m-%d')
         metas=self.__get_meta_data(mp,fd,to,inputs)
         datas=[]
+
         for r in metas:
             rr=self.__get_raw_data(mp,r['ranking_raw_id'])
             r=RankingInfo(rr)
@@ -77,6 +81,8 @@ class Ranking(object):
         inputs=web.input()
         mp=web.ctx.mongo
         datas=self.__get_ranking(mp,inputs)
+        d['media']=self.media
+        d['field']=self.field
         d['datas']=datas
         return render.ranking(d)
     
@@ -87,10 +93,6 @@ class RankingDate(object):
         dk={"mediatype":m,"fieldtype":f,\
             "fetch_date":{"$lt":to,"$gte":fd}}
         return mp.find_all(mp.RANKING_META_DATA,dk)
-    def __get_raw_data(self,mp,raw_id):
-        dk={'_id':raw_id}
-        raw=mp.is_exists(mp.RANKING_RAW_DATA,dk)
-        return raw
 
     def __get_ranking(self,mp,inputs):
         fd=inputs['from']
@@ -98,18 +100,28 @@ class RankingDate(object):
         fd=datetime.strptime(fd,'%Y-%m-%d')
         to=datetime.strptime(to,'%Y-%m-%d')
         metas=self.__get_meta_data(mp,fd,to,inputs)
-        datas=[]
-        for r in metas:
-            rr=self.__get_raw_data(mp,r['ranking_raw_id'])
-            r=RankingInfo(rr)
-            if r.ranking_data:
-                datas.append(r)
-        return datas
+        dds={}
+        for m in metas:
+            dt=m['fetch_date'].strftime("%Y-%m-%d")
+            dds.setdefault(dt,0)
+            dds[dt]+=1
+        tdl=timedelta(days=1)
+        date_info={}
+        date_info['media']=inputs['media']
+        date_info['field']=inputs['field']
+        ddsa=[]
+        for d in sorted(dds.keys()):
+            p=datetime.strptime(d,'%Y-%m-%d')
+            n=p+tdl
+            ddsa.append((p,n))
+        date_info['dates']=ddsa
+        return date_info
+
     def GET(self,*args,**keys):
         d={}
         inputs=web.input()
         mp=web.ctx.mongo
         datas=self.__get_ranking(mp,inputs)
-        d['datas']=datas
-        return render.ranking(d)
+        d['data']=datas
+        return render.date(d)
     
